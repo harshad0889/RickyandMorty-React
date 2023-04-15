81284d2d37c646df09ac8dc17b22382b2dd06578
harshad.hidile <harshad.hidile@gmail.com>
2023-04-15 13:39:23 +0530

    charecter list design and card item design completed

---
+++ src/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./card.css";
+
+const Card = () => {
+  return <div className="card">
+    <img src="/assets/img/char1.jpg" className="char_image" />
+    <h3>Ricky</h3>
+    <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available</p>
+  </div>;
+};
+
+export default Card;
--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import './home.css'
+import React from "react";
+import "./home.css";
+import { useState } from "react";
+import Card from "../Card/Card";
 
-function HomePage() {
+
+const  HomePage=()=> {
+const [charecter,setCharecter] = useState(["AA","BB","CC","DD","EE","FF"])
+
   return (
-    <div className='home_container'>
-      <div className='content_container'>
-
-    
-        <div className='search_box'>
-          
-            <p>search your ricky and morty here...</p>
-            <div className='input_outer_div'>
-              <input className='input' type='text' placeholder='serach your charecter '/>
-              <button className='btn'>Search</button>
-            </div>
-          
+    <div className="home_container">
+      <div className="content_container">
+        <div className="search_box">
+          <p>Find your favourite ricky and morty charecter..</p>
+          <div className="input_outer_div">
+            <input
+              className="input"
+              type="text"
+              placeholder="serach your charecter "
+            />
+            <button className="btn">Search</button>
+          </div>
         </div>
 
-        <div className='card_div'>cards area</div>
+        <div className="card_section">
+          <p>Charecters</p>
+          <div className="card_div">
+          {charecter.map(()=>
+          <Card></Card>)}
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
