dadc71ebc3349d915111fa62014e7d48d9d9ed1d
harshad.hidile <harshad.hidile@gmail.com>
2023-04-15 12:20:17 +0530

    search bar done

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -4,9 +4,21 @@ import './home.css'
 function HomePage() {
   return (
     <div className='home_container'>
-        <div className='search_box'>search</div>
+      <div className='content_container'>
+
+    
+        <div className='search_box'>
+          
+            <p>search your ricky and morty here...</p>
+            <div className='input_outer_div'>
+              <input className='input' type='text' placeholder='serach your charecter '/>
+              <button className='btn'>Search</button>
+            </div>
+          
+        </div>
 
         <div className='card_div'>cards area</div>
+        </div>
     </div>
   )
 }
